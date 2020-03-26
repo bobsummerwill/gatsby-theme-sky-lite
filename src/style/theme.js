b4b8ed4464b5d3828389438463920fe2fb8b087b
Bob Summerwill <bob@summerwill.net>
2020-03-26 14:44:42 -0700

    Switch background to emerald green.

--- src/style/theme.js
+++ src/style/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   palette: {
     primary: {
-      main: "#1f55ff"
+      main: "#1a804a"
     },
     secondary: {
       main: "#6902bf"
